5939526516b104178d5b7383483936415cc3382e
Nadeen <nadeen@ualberta.ca>
2021-11-21 17:39:04 -0700

    pushing changes to clear local directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <div className="App">
+            <header className="App-header">
+              <div className="some-rectangle">
+                <h1>Wadika Faisal</h1>
+              </div>
+              <p>About me!!! Welcome to my life homies!!</p>
+            </header>
+          </div>
+        </Route>
+        <Route path="/users"></Route>
+        <Route path="/"></Route>
+      </Switch>
+    </Router>
   );
 }
 
